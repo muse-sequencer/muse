138b8ce87010a3679565c73d7340770545f1d729
Tim E. Real <termtech@rogers.com>
2010-11-28 21:14:50 +0000

    Added AWL files

---
+++ awl/panentry.h
@@ -0,0 +1,40 @@
+//=============================================================================
+//  Awl
+//  Audio Widget Library
+//  $Id:$
+//
+//  Copyright (C) 2002-2006 by Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+#ifndef __AWLPANENTRY_H__
+#define __AWLPANENTRY_H__
+
+#include "floatentry.h"
+
+namespace Awl {
+
+//---------------------------------------------------------
+//   PanEntry
+//---------------------------------------------------------
+
+class PanEntry : public FloatEntry {
+      Q_OBJECT
+
+   public:
+      PanEntry(QWidget*);
+      };
+}
+
+#endif
