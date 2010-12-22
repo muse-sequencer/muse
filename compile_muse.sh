5bbc2e27e0756318ff29e0595e290be7c27a8414
Robert Jonsson <spamatica@gmail.com>
2010-12-22 13:37:44 +0000

    beatify

--- compile_muse.sh
+++ compile_muse.sh
@@ -7,5 +7,5 @@ else
 	mkdir build
 fi
 cd build
-cmake -DCMAKE_BUILD_TYPE=release .. && make && echo "\nBuild was OK, now enter the 'build' dir and run 'make install' as root"
+cmake -DCMAKE_BUILD_TYPE=release .. && make && echo "Build was OK, now enter the 'build' dir and run 'make install' as root"
 
