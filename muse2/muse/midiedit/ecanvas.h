ef0aa8834d71587f7c417bb36d865ed1c8919a0b
Florian Jung <flo@windfisch.org>
2011-05-24 10:13:03 +0000

    - removed unneccessary "midi in" button from piano- and drum canvas - removed unused step-record button from drum canvas - moved _steprec member from EventCanvas to PianoCanvas

--- muse2/muse/midiedit/ecanvas.h
+++ muse2/muse/midiedit/ecanvas.h
@@ -51,8 +51,6 @@ class EventCanvas : public Canvas {
       MidiEditor* editor;
       unsigned start_tick, end_tick;
       int curVelo;
-      bool _steprec;
-      bool _midiin;
 
       void updateSelection();
       virtual void addItem(Part*, Event&) = 0;
@@ -61,8 +59,6 @@ class EventCanvas : public Canvas {
 
    public slots:
       void redrawGrid()       { redraw(); }
-      void setSteprec(bool f) { _steprec = f; }
-      void setMidiin(bool f)  { _midiin = f; }
 
    signals:
       void pitchChanged(int);       // current cursor position
@@ -75,8 +71,6 @@ class EventCanvas : public Canvas {
       MidiTrack* track() const;
       unsigned start() const       { return start_tick; }
       unsigned end() const         { return end_tick; }
-      bool midiin() const     { return _midiin; }
-      bool steprec() const    { return _steprec; }
       QString getCaption() const;
       void songChanged(int);
       void range(int* s, int* e) const { *s = start_tick; *e = end_tick; }
