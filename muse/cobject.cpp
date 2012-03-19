786cc7c6b68a7f7745d1652522ee06837824d52f
Florian Jung <flo@windfisch.org>
2012-03-19 14:37:25 +0000

    nothing

--- muse/cobject.cpp
+++ muse/cobject.cpp
@@ -167,7 +167,7 @@ void TopWin::readStatus(MusECore::Xml& xml)
 					if (mdisubwin)
 					{
 						if(mdisubwin->isMaximized())
-						  mdisubwin->showNormal();
+							mdisubwin->showNormal();
 						mdisubwin->move(x, y);
 						mdisubwin->resize(width, height);
 					}
