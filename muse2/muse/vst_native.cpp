bfa869ee23aa5da09f18d930766f1d6321854c1b
Tim E. Real <termtech@rogers.com>
2012-12-12 02:22:17 +0000

    Fixes...

--- muse2/muse/vst_native.cpp
+++ muse2/muse/vst_native.cpp
@@ -961,11 +961,11 @@ VstIntPtr VstNativeSynthIF::hostCallback(VstInt32 opcode, VstInt32 index, VstInt
                     p.mbt(&p_bar, &p_beat, &p_tick);
                     
 //#ifndef VST_VESTIGE_SUPPORT
-                    _timeInfo.barStartPos = Pos(p_bar, 0, 0).tick();
-                    _timeInfo.ppqPos = MusEGlobal::audio->tickPos() / 240.0;
+                    _timeInfo.barStartPos = (double)Pos(p_bar, 0, 0).tick() / (double)MusEGlobal::config.division;
+                    _timeInfo.ppqPos = (double)MusEGlobal::audio->tickPos() / (double)MusEGlobal::config.division;
 // #else
-//                     *((double*)&_timeInfo.empty2[0]) = (double)Pos(p_bar, 0, 0).tick() / 120.0;
-//                     *((double*)&_timeInfo.empty1[8]) = (double)MusEGlobal::audio->tickPos() / 120.0;  
+//                     *((double*)&_timeInfo.empty2[0]) = (double)Pos(p_bar, 0, 0).tick() / (double)MusEGlobal::config.division;
+//                     *((double*)&_timeInfo.empty1[8]) = (double)MusEGlobal::audio->tickPos() / (double)MusEGlobal::config.division;
 // #endif
                     //pos->bar++;
                     //pos->beat++;
