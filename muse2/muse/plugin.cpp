57069eac7afbe578c83958dde3c0882feec81264
Andrew Deryabin <andrewderyabin@gmail.com>
2014-10-13 08:09:28 +0400

    merged with master

--- muse2/muse/plugin.cpp
+++ muse2/muse/plugin.cpp
@@ -30,32 +30,33 @@
 #include <math.h>
 #include <sys/stat.h>
 
-#include <QButtonGroup>
-#include <QCheckBox>
-#include <QInputDialog>
-#include <QComboBox>
-#include <QCursor>
-#include <QDir>
-#include <QFile>
+//#include <QButtonGroup>
+//#include <QCheckBox>
+//#include <QInputDialog>
+//#include <QCursor>
+//#include <QDir>
+//#include <QFile>
 #include <QGridLayout>
-#include <QGroupBox>
-#include <QHBoxLayout>
-#include <QHeaderView>
+//#include <QGroupBox>
+//#include <QHBoxLayout>
+//#include <QHeaderView>
 #include <QLabel>
-#include <QMainWindow>
-#include <QMessageBox>
-#include <QPushButton>
-#include <QRadioButton>
-#include <QSignalMapper>
-#include <QSizePolicy>
+//#include <QMainWindow>
+//#include <QPushButton>
+//#include <QRadioButton>
+//#include <QSignalMapper>
+//#include <QSizePolicy>
 #include <QScrollArea>
-#include <QSpacerItem>
+//#include <QSpacerItem>
 #include <QTimer>
-#include <QToolBar>
-#include <QToolButton>
-#include <QTreeWidget>
-#include <QVBoxLayout>
+//#include <QToolButton>
+//#include <QTreeWidget>
+//#include <QVBoxLayout>
+#include <QComboBox>
 #include <QWhatsThis>
+#include <QSignalMapper>
+#include <QToolBar>
+#include <QMessageBox>
 
 #include "globals.h"
 #include "globaldefs.h"
@@ -72,8 +73,8 @@
 #include "fastlog.h"
 #include "checkbox.h"
 #include "verticalmeter.h"
-#include "popupmenu.h"
-#include "menutitleitem.h"
+//#include "popupmenu.h"
+//#include "menutitleitem.h"
 
 #include "lv2host.h"
 
@@ -96,13 +97,7 @@ QList<QString> plugin_group_names;
 
 }
 
-namespace MusEGui {
-int PluginDialog::selectedPlugType = 0;
-int PluginDialog::selectedGroup = 0;
-QStringList PluginDialog::sortItems = QStringList();
-QRect PluginDialog::geometrySave = QRect();
-QByteArray PluginDialog::listSave = QByteArray();
-}
+
 
 namespace MusECore {
 
@@ -2581,7 +2576,7 @@ void PluginI::apply(unsigned pos, unsigned long n, unsigned long ports, float** 
         controls[k].tmpVal = controls[k].val;  // Special for plugins: Deal with tmpVal.
 
 #ifdef PLUGIN_DEBUGIN_PROCESS
-        printf("PluginI::apply k:%lu sample:%lu nextFrame:%d nsamp:%lu val:%f\n", k, sample, ci.eFrame, nsamp, controls[k].val);
+        printf("PluginI::apply k:%lu sample:%lu frame:%lu nextFrame:%d nsamp:%lu \n", k, sample, frame, ci.eFrame, nsamp);
 #endif
       }
     }
@@ -2905,488 +2900,6 @@ int PluginI::oscControl(unsigned long port, float value)
 
 namespace MusEGui {
 
-//---------------------------------------------------------
-//   PluginDialog
-//    select Plugin dialog
-//---------------------------------------------------------
-
-PluginDialog::PluginDialog(QWidget* parent)
-  : QDialog(parent)
-      {
-      group_info=NULL;
-      setWindowTitle(tr("MusE: select plugin"));
-
-      if(!geometrySave.isNull())
-        setGeometry(geometrySave);
-
-      QVBoxLayout* layout = new QVBoxLayout(this);
-
-      tabBar = new QTabBar(this);
-      tabBar->setToolTip(tr("Plugin categories.\nRight-click on tabs to manage.\nRight-click on plugins to add/remove from a category."));
-      tabBar->addTab("All");
-      for (QList<QString>::iterator it=MusEGlobal::plugin_group_names.begin(); it!=MusEGlobal::plugin_group_names.end(); it++)
-        tabBar->addTab(*it);
-
-
-      pList  = new QTreeWidget(this);
-
-      pList->setColumnCount(12);
-      // "Note: In order to avoid performance issues, it is recommended that sorting
-      //   is enabled after inserting the items into the tree. Alternatively, you could
-      //   also insert the items into a list before inserting the items into the tree. "
-      QStringList headerLabels;
-      headerLabels << tr("Type");
-      headerLabels << tr("Lib");
-      headerLabels << tr("Label");
-      headerLabels << tr("Name");
-      headerLabels << tr("AI");
-      headerLabels << tr("AO");
-      headerLabels << tr("CI");
-      headerLabels << tr("CO");
-      headerLabels << tr("IP");
-      headerLabels << tr("id");
-      headerLabels << tr("Maker");
-      headerLabels << tr("Copyright");
-
-      pList->setHeaderLabels(headerLabels);
-
-      pList->headerItem()->setToolTip(4,  tr("Audio inputs"));
-      pList->headerItem()->setToolTip(5,  tr("Audio outputs"));
-      pList->headerItem()->setToolTip(6,  tr("Control inputs"));
-      pList->headerItem()->setToolTip(7,  tr("Control outputs"));
-      pList->headerItem()->setToolTip(8,  tr("In-place capable"));
-      pList->headerItem()->setToolTip(9,  tr("ID number"));
-
-      pList->setRootIsDecorated(false);
-      pList->setSelectionBehavior(QAbstractItemView::SelectRows);
-      pList->setSelectionMode(QAbstractItemView::SingleSelection);
-      pList->setAlternatingRowColors(true);
-      pList->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-      pList->setContextMenuPolicy(Qt::CustomContextMenu);
-
-      layout->addWidget(tabBar);
-      layout->addWidget(pList);
-
-      //---------------------------------------------------
-      //  Ok/Cancel Buttons
-      //---------------------------------------------------
-
-      QBoxLayout* w5 = new QHBoxLayout;
-      layout->addLayout(w5);
-
-      QBoxLayout* ok_lo = new QVBoxLayout;
-      w5->addLayout(ok_lo);
-
-      okB     = new QPushButton(tr("Ok"), this);
-      okB->setDefault(true);
-      QPushButton* cancelB = new QPushButton(tr("Cancel"), this);
-      okB->setFixedWidth(80);
-      okB->setEnabled(false);
-      cancelB->setFixedWidth(80);
-      ok_lo->addWidget(okB);
-      ok_lo->addSpacing(8);
-      ok_lo->addWidget(cancelB);
-
-      QGroupBox* plugSelGroup = new QGroupBox(this);
-      plugSelGroup->setTitle(tr("Show plugs:"));
-      plugSelGroup->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
-      QGridLayout* psl = new QGridLayout;
-      plugSelGroup->setLayout(psl);
-
-      QButtonGroup* plugSel = new QButtonGroup(plugSelGroup);
-      onlySM  = new QRadioButton(this);
-      onlySM->setText(tr("Mono and Stereo"));
-      onlySM->setCheckable(true);
-      plugSel->addButton(onlySM);
-      psl->addWidget(onlySM, 1, 0);
-      onlyS = new QRadioButton(this);
-      onlyS->setText(tr("Stereo"));
-      onlyS->setCheckable(true);
-      plugSel->addButton(onlyS);
-      psl->addWidget(onlyS, 0, 1);
-      onlyM = new QRadioButton(this);
-      onlyM->setText(tr("Mono"));
-      onlyM->setCheckable(true);
-      plugSel->addButton(onlyM);
-      psl->addWidget(onlyM, 0, 0);
-      allPlug = new QRadioButton(this);
-      allPlug->setText(tr("Show All"));
-      allPlug->setCheckable(true);
-      plugSel->addButton(allPlug);
-      psl->addWidget(allPlug, 1, 1);
-      plugSel->setExclusive(true);
-
-      switch(selectedPlugType) {
-            case SEL_SM:  onlySM->setChecked(true);  break;
-            case SEL_S:   onlyS->setChecked(true);   break;
-            case SEL_M:   onlyM->setChecked(true);   break;
-            case SEL_ALL: allPlug->setChecked(true); break;
-            }
-
-      tabBar->setCurrentIndex(selectedGroup);
-      tabBar->setContextMenuPolicy(Qt::ActionsContextMenu);
-      newGroupAction= new QAction(tr("&create new group"),tabBar);
-      delGroupAction= new QAction(tr("&delete currently selected group"),tabBar);
-      renGroupAction= new QAction(tr("re&name currently selected group"),tabBar);
-      tabBar->addAction(newGroupAction);
-      tabBar->addAction(delGroupAction);
-      tabBar->addAction(renGroupAction);
-
-      if (selectedGroup==0)
-      {
-				delGroupAction->setEnabled(false);
-				renGroupAction->setEnabled(false);
-			}
-      //tabBar->setMovable(true); //not yet. need to find a way to forbid moving the zeroth tab
-
-
-      plugSelGroup->setToolTip(tr("Select which types of plugins should be visible in the list.<br>"
-                             "Note that using mono plugins on stereo tracks is not a problem, two will be used in parallel.<br>"
-                             "Also beware that the 'all' alternative includes plugins that may not be useful in an effect rack."));
-
-      w5->addSpacing(8);
-      w5->addWidget(plugSelGroup);
-      w5->addSpacing(8);
-
-      QBoxLayout* srch_lo = new QVBoxLayout;
-      w5->addLayout(srch_lo);
-
-      QLabel *sortLabel = new QLabel(this);
-      sortLabel->setText(tr("Search in 'Label' and 'Name':"));
-      srch_lo->addSpacing(8);
-      srch_lo->addWidget(sortLabel);
-      srch_lo->addSpacing(8);
-
-      sortBox = new QComboBox(this);
-      sortBox->setEditable(true);
-      if (!sortItems.empty())
-            sortBox->addItems(sortItems);
-
-      sortBox->setMinimumSize(100, 10);
-      srch_lo->addWidget(sortBox);
-      // FIXME: Adding this makes the whole bottom hlayout expand. Would like some space between lineedit and bottom.
-      //        Same thing if spacers added to group box or Ok Cancel box.
-      //srch_lo->addSpacerItem(new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Maximum));
-
-      fillPlugs();
-
-      pList->setSortingEnabled(true);
-
-      if(listSave.isEmpty())
-      {
-        int sizes[] = { 80, 110, 110, 110, 30, 30, 30, 30, 30, 50, 110, 110 };
-        for (int i = 0; i < 12; ++i) {
-              if (sizes[i] <= 50)     // hack alert!
-                    pList->header()->setResizeMode(i, QHeaderView::Fixed);
-              pList->header()->resizeSection(i, sizes[i]);
-        }
-        pList->sortByColumn(3, Qt::AscendingOrder);
-      }
-      else
-        pList->header()->restoreState(listSave);
-
-      connect(pList,   SIGNAL(itemDoubleClicked(QTreeWidgetItem*,int)), SLOT(accept()));
-      connect(pList,   SIGNAL(itemClicked(QTreeWidgetItem*,int)), SLOT(enableOkB()));
-      connect(pList,   SIGNAL(customContextMenuRequested(const QPoint&)), SLOT(plistContextMenu(const QPoint&)));
-      connect(cancelB, SIGNAL(clicked()), SLOT(reject()));
-      connect(okB,     SIGNAL(clicked()), SLOT(accept()));
-      connect(plugSel, SIGNAL(buttonClicked(QAbstractButton*)), SLOT(pluginTypeSelectionChanged(QAbstractButton*)));
-      connect(tabBar,  SIGNAL(currentChanged(int)), SLOT(tabChanged(int)));
-      //connect(tabBar,  SIGNAL(tabMoved(int,int)), SLOT(tabMoved(int,int))); //not yet. need to find a way to forbid moving the zeroth tab
-      connect(sortBox, SIGNAL(editTextChanged(const QString&)),SLOT(fillPlugs()));
-      connect(newGroupAction, SIGNAL(activated()), SLOT(newGroup()));
-      connect(delGroupAction, SIGNAL(activated()), SLOT(delGroup()));
-      connect(renGroupAction, SIGNAL(activated()), SLOT(renameGroup()));
-      sortBox->setFocus();
-      }
-
-void PluginDialog::plistContextMenu(const QPoint& point)
-{
-  QTreeWidgetItem* item = pList->currentItem();
-  if (item)
-  {
-    group_info = &MusEGlobal::plugin_groups.get(item->text(1), item->text(2));
-    QMenu* menu = new MusEGui::PopupMenu(this, true);
-    QSignalMapper* mapper = new QSignalMapper(this);
-    menu->addAction(new MusEGui::MenuTitleItem(tr("Associated categories"), menu));
-
-    if (tabBar->count()==1)
-    {
-      QAction* tmp=menu->addAction(tr("You need to define some categories first."));
-      tmp->setEnabled(false);
-    }
-    else
-    {
-      for (int i=1; i<tabBar->count(); i++) // ignore the first tab ("All")
-      {
-        QAction* act=menu->addAction(tabBar->tabText(i));
-        act->setCheckable(true);
-        act->setChecked(group_info->contains(i));
-        connect(act,SIGNAL(toggled(bool)), mapper, SLOT(map()));
-        mapper->setMapping(act, i);
-      }
-      connect(mapper, SIGNAL(mapped(int)), this, SLOT(groupMenuEntryToggled(int)));
-    }
-
-    menu->exec(mapToGlobal(point));
-
-    delete mapper;
-    delete menu;
-
-    if (selectedGroup!=0 && !group_info->contains(selectedGroup)) // we removed the entry from the currently visible group
-      fillPlugs();
-
-    group_info=NULL;
-  }
-}
-
-void PluginDialog::groupMenuEntryToggled(int index)
-{
-  if (group_info)
-  {
-    if (group_info->contains(index))
-      group_info->remove(index);
-    else
-      group_info->insert(index);
-  }
-  else
-  {
-    fprintf(stderr,"THIS SHOULD NEVER HAPPEN: groupMenuEntryToggled called but group_info is NULL!\n");
-  }
-}
-
-
-
-//---------------------------------------------------------
-//   enableOkB
-//---------------------------------------------------------
-
-void PluginDialog::enableOkB()
-{
-  okB->setEnabled(true);
-}
-
-
-void PluginDialog::newGroup()
-{
-  MusEGlobal::plugin_groups.shift_right(selectedGroup+1, tabBar->count());
-  tabBar->insertTab(selectedGroup+1, tr("new group"));
-  MusEGlobal::plugin_group_names.insert(selectedGroup, tr("new group"));
-}
-
-void PluginDialog::delGroup()
-{
-  if (selectedGroup!=0)
-  {
-    MusEGlobal::plugin_groups.erase(selectedGroup);
-    MusEGlobal::plugin_groups.shift_left(selectedGroup+1, tabBar->count());
-    tabBar->removeTab(selectedGroup);
-    MusEGlobal::plugin_group_names.removeAt(selectedGroup-1);
-  }
-}
-
-void PluginDialog::renameGroup()
-{
-  if (selectedGroup!=0)
-  {
-    bool ok;
-    QString newname = QInputDialog::getText(this, tr("Enter the new group name"),
-                                        tr("Enter the new group name"), QLineEdit::Normal,
-                                        tabBar->tabText(selectedGroup), &ok);
-    if (ok)
-    {
-      tabBar->setTabText(selectedGroup, newname);
-      MusEGlobal::plugin_group_names.replace(selectedGroup-1, newname);
-    }
-  }
-}
-
-
-
-
-//---------------------------------------------------------
-//   value
-//---------------------------------------------------------
-
-MusECore::Plugin* PluginDialog::value()
-      {
-      QTreeWidgetItem* item = pList->currentItem();
-      if (item)
-        return MusEGlobal::plugins.find(item->text(1), item->text(2));
-      printf("plugin not found\n");
-      return 0;
-      }
-
-//---------------------------------------------------------
-//   saveSettings
-//---------------------------------------------------------
-
-void PluginDialog::saveSettings()
-{
-  if (!sortBox->currentText().isEmpty()) {
-        bool found = false;
-        foreach (QString item, sortItems)
-            if(item == sortBox->currentText()) {
-                found = true;
-                break;
-                }
-        if(!found)
-          sortItems.push_front(sortBox->currentText());
-        }
-
-  QHeaderView* hdr = pList->header();
-  if(hdr)
-    listSave = hdr->saveState();
-
-  geometrySave = geometry();
-}
-
-//---------------------------------------------------------
-//   accept
-//---------------------------------------------------------
-
-void PluginDialog::accept()
-      {
-      saveSettings();
-      QDialog::accept();
-      }
-
-//---------------------------------------------------------
-//   reject
-//---------------------------------------------------------
-
-void PluginDialog::reject()
-{
-      saveSettings();
-      QDialog::reject();
-}
-
-//---------------------------------------------------------
-//    pluginTypeSelectionChanged
-//---------------------------------------------------------
-
-void PluginDialog::pluginTypeSelectionChanged(QAbstractButton* ab)
-      {
-      if (ab == allPlug)
-            selectedPlugType = SEL_ALL;
-      else if (ab == onlyM)
-            selectedPlugType = SEL_M;
-      else if (ab == onlyS)
-            selectedPlugType = SEL_S;
-      else if (ab == onlySM)
-            selectedPlugType = SEL_SM;
-      fillPlugs();
-      }
-
-void PluginDialog::tabChanged(int index)
-{
-  renGroupAction->setEnabled(index!=0);
-  delGroupAction->setEnabled(index!=0);
-
-  selectedGroup=index;
-  fillPlugs();
-}
-
-void PluginDialog::tabMoved(int from, int to)
-{
-//all the below doesn't work :/
-/*  static bool recurse=false;
-
-  if (!recurse)
-  {
-    if (from==0 && to!=0) {recurse=true; tabBar->moveTab(to, from);}
-    if (from!=0 && to==0) {recurse=true; tabBar->moveTab(from, to);}
-  }
-  recurse=false;*/
-
-
-   //if ((from==0 && to!=0) || (from!=0 && to==0)) { tabBar->setMovable(false); tabBar->setMovable(true); }
-   printf("**** %i -> %i\n", from, to);
-
-  //FINDMICH TODO
-}
-
-void PluginDialog::fillPlugs()
-{
-    QString type_name;
-    pList->clear();
-    okB->setEnabled(false);
-    for (MusECore::iPlugin i = MusEGlobal::plugins.begin(); i != MusEGlobal::plugins.end(); ++i)
-       if (selectedGroup==0 || MusEGlobal::plugin_groups.get(*i).contains(selectedGroup))
-       {
-          unsigned long ai = (*i)->inports();
-          unsigned long ao = (*i)->outports();
-          unsigned long ci = (*i)->controlInPorts();
-          unsigned long co = (*i)->controlOutPorts();
-          bool found = false;
-          QString sb_txt = sortBox->currentText().toLower();
-          if(sb_txt.isEmpty() || (*i)->label().toLower().contains(sb_txt) || (*i)->name().toLower().contains(sb_txt))
-                found = true;
-
-          bool addFlag = false;
-          switch (selectedPlugType) {
-                case SEL_SM: // stereo & mono
-                      if ((ai == 1 || ai == 2) && (ao == 1 || ao ==2)) {
-                            addFlag = true;
-                            }
-                      break;
-                case SEL_S: // stereo
-                      if ((ai == 1 || ai == 2) &&  ao ==2) {
-                            addFlag = true;
-                            }
-                      break;
-                case SEL_M: // mono
-                      if (ai == 1  && ao == 1) {
-                            addFlag = true;
-                            }
-                      break;
-                case SEL_ALL: // all
-                      addFlag = true;
-                      break;
-                }
-          if (found && addFlag) {
-                QTreeWidgetItem* item = new QTreeWidgetItem;
-                if((*i)->isDssiSynth())
-                  type_name = tr("dssi synth");
-                else if((*i)->isDssiPlugin())
-                  type_name = tr("dssi effect");
-				else if((*i)->isLV2Synth())
-                  type_name = tr("LV2 synth");
-                else if((*i)->isLV2Plugin())
-                  type_name = tr("LV2 effect");
-                else
-                  type_name = tr("ladspa");
-                item->setText(0,  type_name);
-                item->setText(1,  (*i)->lib());
-                item->setText(2,  (*i)->label());
-                item->setText(3,  (*i)->name());
-                item->setText(4,  QString().setNum(ai));
-                item->setText(5,  QString().setNum(ao));
-                item->setText(6,  QString().setNum(ci));
-                item->setText(7,  QString().setNum(co));
-                item->setText(8,  QString().setNum((*i)->inPlaceCapable()));
-                item->setText(9,  QString().setNum((*i)->id()));
-                item->setText(10,  (*i)->maker());
-                item->setText(11, (*i)->copyright());
-                pList->addTopLevelItem(item);
-                }
-          }
-}
-
-//---------------------------------------------------------
-//   getPlugin
-//---------------------------------------------------------
-
-MusECore::Plugin* PluginDialog::getPlugin(QWidget* parent)
-      {
-      PluginDialog* dialog = new PluginDialog(parent);
-      MusECore::Plugin* p = 0;
-      int rv = dialog->exec();
-      if(rv)
-        p = dialog->value();
-      delete dialog;
-      return p;
-      }
 
 // TODO: We need to use .qrc files to use icons in WhatsThis bubbles. See Qt
 // Resource System in Qt documentation - ORCAN
