5f65d4cb5c71bb0eca0fa7fb7a1044f68ff7b291
kybos <kybos@web.de>
2020-09-28 15:32:07 +0200

    Display number of parts per editor if > 1

--- muse3/muse/app.cpp
+++ muse3/muse/app.cpp
@@ -2289,7 +2289,7 @@ void MusE::startPianoroll(MusECore::PartList* pl, bool showDefaultCtrls)
       {
       MusEGui::PianoRoll* pianoroll = new MusEGui::PianoRoll(pl, this, nullptr, _arranger->cursorValue(), showDefaultCtrls);
       toplevels.push_back(pianoroll);
-      pianoroll->show();
+//      pianoroll->show(); // should be redundant, done in ctor
       connect(pianoroll, SIGNAL(isDeleting(MusEGui::TopWin*)), SLOT(toplevelDeleting(MusEGui::TopWin*)));
       connect(MusEGlobal::muse, SIGNAL(configChanged()), pianoroll, SLOT(configChanged()));
       updateWindowMenu();
