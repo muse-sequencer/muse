a769b2ece4294bec0d502ef4d301fd932b6197cd
Florian Walpen <dev@submerge.ch>
2022-07-26 16:09:50 +0200

    Replace std::random_shuffle for C++17 compliance.
    The std::random_shuffle algorithm has been removed in C++17, see:
    
    https://en.cppreference.com/w/cpp/algorithm/random_shuffle
    
    Build fails with compilers that actually enforce C++17 compliance.
    Replace this code with the newer std::shuffle algorithm.

--- src/muse/app.cpp
+++ src/muse/app.cpp
@@ -49,6 +49,7 @@
 #include <iostream>
 #include <algorithm>
 //#include <typeinfo>
+#include <random>
 
 #include "app.h"
 #include "master/lmaster.h"
@@ -2743,7 +2744,8 @@ void MusE::showDidYouKnowDialog()
       didYouKnow.tipList.append(tipMessage);
     }
 
-    std::random_shuffle(didYouKnow.tipList.begin(),didYouKnow.tipList.end());
+    std::random_device randomDevice;
+    std::shuffle(didYouKnow.tipList.begin(), didYouKnow.tipList.end(), randomDevice);
 
     didYouKnow.show();
     if( didYouKnow.exec()) {
